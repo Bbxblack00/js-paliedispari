1364dcf8d6e7229092c82c97eedec27deb6194b5
Benito <bbxblack@gmail.com>
2021-03-12 17:29:51 +0100

    v1.0

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 // palindromo
 
 // var paliUser = prompt('Inserisci un palindromo: ');
-
-function paliEx(pali1) {
-  var pali2 = [];
-  var temp;
-  for (var x = pali1.length - 1; x >= 0 ; x--) {
-    temp = pali1[x];
-    pali2.push(temp);
-  }
-  for (var i = 0; i < pali1.length; i++) {
-    if (pali1[i] === pali2[i]) {
-      return 'la frase è palindroma';
-    } else {
-      return 'la frase non è palindroma'
-    }
-  }
-}
-
-// console.log(paliEx(paliUser));
+//
+// function paliEx(pali1) {
+//   var pali2 = [];
+//   var temp;
+//   for (var x = pali1.length - 1; x >= 0 ; x--) {
+//     temp = pali1[x];
+//     pali2.push(temp);
+//   }
+//   for (var i = 0; i < pali1.length; i++) {
+//     console.log(i);
+//     if (pali1[i] === pali2[i]) {
+//
+//     } else {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//
+// var result = paliEx(paliUser);
+//
+// if (result) {
+//   console.log('La frase è palindroma');
+// } else {
+//   console.log('La frase non è palindroma');
+// }
 
 
 
@@ -28,29 +36,44 @@ function paliEx(pali1) {
 
 var parOddUser = prompt('Scegli "pari" o "dispari": ');
 var numUser = parseInt(prompt('Inserisci un numero da 1 a 5: '));
+var min = 1;
+var max = 5;
 
 if (numUser > 5 || numUser < 0) {
   alert('eeeeeeeeeeeeeeeeeh volevi!!!');
 } else {
-  function numRnd() {
-    var numPc = Math.floor(Math.random() * 5) + 1;
-    return numPc;
+
+
+  
+
+  var varNumRnd = numRnd(min, max);
+  var sum = varNumRnd + numUser;
+
+
+
+  console.log(varNumRnd);
+  console.log(mach(sum));
+
+}
+
+
+
+// qui iniziano le funzioni
+
+function numRnd(minRnd, maxRnd) {
+  maxRnd -= minRnd + 1;
+  var numPc = Math.floor(Math.random() * maxRnd) + minRnd;
+  return numPc;
+}
+
+function mach(summ){
+
+  if (parOddUser == 'pari' && summ % 2 == 0) {
+    return 'Hai vinto ' + summ;
+  } else if (parOddUser == 'dispari' && summ % 2 == 1) {
+    return 'Hai vinto ' + summ;
+  } else {
+    return 'Hai perso ' + summ;
   }
-
-  var sum = numRnd() + numUser;
-
-  function mach(){
-
-    if (parOddUser == 'pari' && sum % 2 == 0) {
-      return 'Hai vinto ' + sum;
-    } else if (parOddUser == 'dispari' && sum % 2 == 1) {
-      return 'Hai vinto ' + sum;
-    } else {
-      return 'Hai perso ' + sum;
-    }
-
-  }
-
-  console.log(mach());
 
 }
